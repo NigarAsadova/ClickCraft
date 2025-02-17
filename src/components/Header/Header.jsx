5c6079a92a9d51c3dc1b4be8af42ac2178fb28b7
NigarAsadova <nigar.asadova555@gmail.com>
2025-02-17 23:39:19 +0400

    Add Home, Header, and Menu pages; implement Home page functionality

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import Logo from "../../assets/icons/main-logo.svg"
+const Header = () => {
+    return ( 
+        <div className="max-w-[1720px] mx-auto pt-7 pb-9">
+            <div>
+                <div>
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <div></div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+
+const Home = () => {
+    return (  
+        <div  className=" bg-[#010314]">
+            <Header />
+        </div>
+    );
+}
+ 
+export default Home;
