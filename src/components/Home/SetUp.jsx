b29d3f715665f262f472d0d679c972bacc482477
NigarAsadova <nigar.asadova555@gmail.com>
2025-02-21 08:39:25 +0400

    Complete "5 minute set-up process", change button's display to inline-block

--- src/components/Home/SetUp.jsx
+++ src/components/Home/SetUp.jsx
@@ -4,24 +4,27 @@ import star from "../../assets/icons/stars.svg";
 import saly from "../../assets/imgs/Saly-13.svg";
 const SetUp = () => {
     return ( 
-        <div>
+        <div className="max-w-[1520px] mx-auto">
             <div className="max-w-[746px] mx-auto">
                 <SectionTitle title="5 minute set-up process" />
                 <p className="font-normal text-2xl text-center text-[#77798F]">Just take <span className="font-bold">5 minutes</span> to fill in some info, choose a killer template, and bam! Your personalized portfolio website is ready.</p>                
             </div>
-            <div className="border border-[#664DFF]">
-                <div className="border border-[#2A2B3A] rounded-[50px]">
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <div className="flex items-center gap-3 mb-5">
-                        <div><img src={star} alt="stars" /></div>
-                        <span className="bg-custom-blue-gradient bg-clip-text text-transparent text-base font-medium uppercase tracking-[0.25em] ">How It Works</span>
+            <div className="border-t border-b-[3px] rounded-[42px] border-[#664DFF] p-[50px] shadow-purple-box-shadow mt-[68px]">
+                <div className="grid grid-cols-2 gap-[106px] items-center">
+                    <div className="border border-[#2A2B3A] rounded-[50px] bg-saly-gradient flex justify-center">
+                        <img src={saly} alt="saly" />
                     </div>
-                    <h3 className="font-bold text-[44px] leading-[52.8px] bg-custom-white-gradient bg-clip-text text-transparent">Complete Your Profile</h3>
-                    <p className="my-6 font-normal text-[26px] text-[#77798F] leading-[41.6px]"> Just share your details, choose a template, and see your personal portfolio magically appear. Confirm or make any changes you like, and  Grab a special link to share your portfolio with everyone. Boost your online presence the easy way!</p>
-                    <Button text="Complete Your Profile" />
+                    <div className="max-w-[615px]">
+                        <div className="flex items-center gap-3 mb-5">
+                            <div><img src={star} alt="stars" /></div>
+                            <span className="bg-custom-blue-gradient bg-clip-text text-transparent text-base font-medium uppercase tracking-[0.25em] ">How It Works</span>
+                        </div>
+                        <h3 className="font-bold text-[44px] leading-[52.8px] bg-custom-white-gradient bg-clip-text text-transparent">Complete Your Profile</h3>
+                        <p className="my-6 font-normal text-[26px] text-[#77798F] leading-[41.6px]"> Just share your details, choose a template, and see your personal portfolio magically appear. Confirm or make any changes you like, and  Grab a special link to share your portfolio with everyone. Boost your online presence the easy way!</p>
+                        <Button text="Complete Your Profile" to="#" />
+                    </div>                    
                 </div>
+
             </div>
         </div>
      );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     extend: {
       boxShadow: {
         'custom-purple': '0px 0px 10px 0px rgba(114,65,255,1)',
+        'purple-box-shadow' : '0px 4px 100px 0px #664DFF33',
       },
       backgroundImage: {
         'custom-blue-gradient': "linear-gradient(90deg, rgba(98,127,255,1) 0%, rgba(134,91,255,1) 45%)",
         'custom-white-gradient' : "radial-gradient(50% 50% at 50% 50%, #FFFFFF 30%, rgba(255, 255, 255, 0.5) 84.77%)",
+        'saly-gradient' : "radial-gradient(217.23% 100.08% at 50% 18.33%, rgba(0, 0, 0, 0) 21.67%, rgba(102, 51, 238, 0.5) 69.34%, rgba(255, 255, 255, 0.5) 100%)",
       },
       fontFamily:{
         'Poppins' : "'Poppins', serif",
