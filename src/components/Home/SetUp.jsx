18a2d3a79d14ce0dd721b91b98ed1e623141f477
NigarAsadova <nigar.asadova555@gmail.com>
2025-02-24 23:07:19 +0400

    update grid columns for responsive image and text display

--- src/components/Home/SetUp.jsx
+++ src/components/Home/SetUp.jsx
@@ -10,13 +10,15 @@ const SetUp = () => {
                 <p className="font-normal text-2xl text-center text-[#77798F]">Just take <span className="font-bold">5 minutes</span> to fill in some info, choose a killer template, and bam! Your personalized portfolio website is ready.</p>                
             </div>
             <div className="border-t border-b-[3px] rounded-[42px] border-[#664DFF] p-[50px] shadow-purple-box-shadow mt-[68px]">
-                <div className="grid grid-cols-2 gap-[106px] items-center">
-                    <div className="border border-[#2A2B3A] rounded-[50px] bg-saly-gradient flex justify-center">
+                <div className="grid grid-cols-2 gap-[106px] items-center max-lg:grid-cols-1 max-lg:flex max-lg:flex-col-reverse">
+                    <div className="border border-[#2A2B3A] rounded-[50px] min-lg:bg-saly-gradient flex justify-center max-lg:border-0">
                         <img src={saly} alt="saly" />
                     </div>
                     <div className="max-w-[615px]">
+                        <div className="max-lg:hidden">
                         <HowItWorks />
                         <h3 className="font-bold text-[44px] leading-[52.8px] bg-custom-white-gradient bg-clip-text text-transparent">Complete Your Profile</h3>
+                        </div> 
                         <p className="my-6 font-normal text-[26px] text-[#77798F] leading-[41.6px]"> Just share your details, choose a template, and see your personal portfolio magically appear. Confirm or make any changes you like, and  Grab a special link to share your portfolio with everyone. Boost your online presence the easy way!</p>
                         <Button text="Complete Your Profile" to="#" />
                     </div>                    
