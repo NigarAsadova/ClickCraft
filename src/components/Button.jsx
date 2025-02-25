56e526f1ab8cad172ee83e10b227fbddb7a82fb2
NigarAsadova <nigar.asadova555@gmail.com>
2025-02-25 08:31:26 +0400

    Make 5-minute setup part responsive and change font-size of button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Button = ({ text, to }) => {
     return ( 
         <Link to={to}>
-            <div className="border-[3px] border-[#2A2B3A] rounded-[40px] py-4 px-10 font-medium text-base leading-[18.75px] text-white hover:border-[rgb(114,65,255)] hover:shadow-custom-purple transition-all ease-in-out duration-500 inline-block max-lg:flex max-lg:items-center max-lg:justify-center max-lg:text-center max-lg:min-h-[65px]">
+            <div className="border-[3px] border-[#2A2B3A] rounded-[40px] py-4 px-10 font-medium text-base max-md:text-lg leading-[18.75px] text-white hover:border-[rgb(114,65,255)] hover:shadow-custom-purple transition-all ease-in-out duration-500 inline-block max-lg:flex max-lg:items-center max-lg:justify-center max-lg:text-center max-lg:min-h-[65px]">
                 {text}
             </div>
         </Link>
--- src/components/Home/SetUp.jsx
+++ src/components/Home/SetUp.jsx
@@ -4,13 +4,13 @@ import saly from "../../assets/imgs/Saly-13.svg";
 import HowItWorks from "./HowItWorks";
 const SetUp = () => {
     return ( 
-        <div className="max-w-[1520px] mx-auto mt-[135px] max-lg:text-center">
+        <div className="max-w-[1520px] mx-auto mt-[135px] max-md:mt-[50px] max-lg:text-center">
             <div className="max-w-[746px] mx-auto">
                 <SectionTitle title="5 minute set-up process" />
-                <p className="font-normal text-2xl text-center text-[#77798F]">Just take <span className="font-bold">5 minutes</span> to fill in some info, choose a killer template, and bam! Your personalized portfolio website is ready.</p>                
+                <p className="font-normal text-2xl text-center text-[#77798F] max-md:hidden">Just take <span className="font-bold">5 minutes</span> to fill in some info, choose a killer template, and bam! Your personalized portfolio website is ready.</p>                
             </div>
-            <div className="border-t border-b-[3px] rounded-[42px] border-[#664DFF] p-[50px] shadow-purple-box-shadow mt-[68px]">
-                <div className="grid grid-cols-2 gap-[106px] items-center max-lg:grid-cols-1 max-lg:flex max-lg:flex-col-reverse">
+            <div className="border-t border-b-[3px] rounded-[42px] border-[#664DFF] p-[50px] max-md:pt-[14px] max-md:px-[18px] max-md:pb-2 shadow-purple-box-shadow mt-[68px] max-md:mt-0">
+                <div className="grid grid-cols-2 gap-[106px] max-md:gap-5 items-center max-lg:grid-cols-1 max-lg:flex max-lg:flex-col-reverse">
                     <div className="border border-[#2A2B3A] rounded-[50px] min-lg:bg-saly-gradient flex justify-center max-lg:border-0">
                         <img src={saly} alt="saly" />
                     </div>
@@ -19,7 +19,7 @@ const SetUp = () => {
                         <HowItWorks />
                         <h3 className="font-bold text-[44px] leading-[52.8px] bg-custom-white-gradient bg-clip-text text-transparent">Complete Your Profile</h3>
                         </div> 
-                        <p className="my-6 font-normal text-[26px] text-[#77798F] leading-[41.6px]"> Just share your details, choose a template, and see your personal portfolio magically appear. Confirm or make any changes you like, and  Grab a special link to share your portfolio with everyone. Boost your online presence the easy way!</p>
+                        <p className="my-6 font-normal text-[26px] max-md:text-base text-[#77798F] leading-[41.6px]"> Just share your details, choose a template, and see your personal portfolio magically appear. Confirm or make any changes you like, and  Grab a special link to share your portfolio with everyone. Boost your online presence the easy way!</p>
                         <Button text="Complete Your Profile" to="#" />
                     </div>                    
                 </div>
