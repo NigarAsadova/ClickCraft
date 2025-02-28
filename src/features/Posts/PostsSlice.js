7d42fcf6c915a93717158e911a2fd8be98d99b5c
NigarAsadova <nigar.asadova555@gmail.com>
2025-02-28 11:28:40 +0400

    Created PostsSlice to manage posts state (loading, error, posts).

---
+++ src/features/Posts/PostsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getPost } from "./PostsApi";
+
+const initialState = {
+    error : null,
+    isLaoding : false,
+    posts : []
+};
+
+export const PostsSlice = createSlice({
+    name : "posts",
+    initialState,
+    reducers:{},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getPost.pending, (state) =>{
+                state.isLaoding = true;
+                state.error = null;
+            })
+            .addCase(getPost.fulfilled, (state, action) => {
+                state.isLaoding = false;
+                state.post = action.payload;
+            })
+            .addCase(getPost.rejected, (state, action) => {
+                state.isLaoding = false;
+                state.error = action.payload;
+            })
+    }
+})
