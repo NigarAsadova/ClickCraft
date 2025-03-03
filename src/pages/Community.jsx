29afcccd607aec9a57b3978e328a8e7a1647b4e2
NigarAsadova <nigar.asadova555@gmail.com>
2025-03-03 22:53:39 +0400

    Create Community page and its structure

---
+++ src/pages/Community.jsx
@@ -0,0 +1,22 @@
+const Community = () => {
+    return ( 
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <p>Author of "Python 3.0 for Beginners" and a Full Stack Developer passionate about creating dynamic and user-friendly web experiences.</p>
+                            <div>
+                                
+                            </div>
+                        </div>
+                        <div></div>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Community;
