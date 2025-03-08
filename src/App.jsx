dda4e70601c1cacc0423007a58c39d9a4e432d34
NigarAsadova <nigar.asadova555@gmail.com>
2025-03-08 23:07:48 +0400

    Add content of Community page and its router into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer';
 import Portfolios from './pages/Portfolios';
+import Community from './pages/Community';
 function App() {
   return (
     <div className='bg-[#010314] px-6'>
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/portfolios' element={<Portfolios />} />
+        <Route path='/community' element={<Community />} />
       </Routes>
       <Footer />
     </Router>
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,17 +1,23 @@
+import eye from "../assets/icons/Eye.svg"
+
 const Community = () => {
     return ( 
         <div>
             <div>
                 <div>
-                    <div>
-                        <div>
-                            <p>Author of "Python 3.0 for Beginners" and a Full Stack Developer passionate about creating dynamic and user-friendly web experiences.</p>
+                    <div className="p-5 border-[#664DFF] border-t-2 border-x border-b-4 rounded-[10px] flex flex-col items-">
+                        <div className="text-[#77798F]">
+                            Author of "Python 3.0 for Beginners" and a Full Stack Developer passionate about creating dynamic and user-friendly web experiences.
+                        </div>
+                        <div className="text-white">
+                            <p>Portfolios <span>10</span></p>
                             <div>
-                                
+                                <div className="flex gap-[6px]"><div><img src={eye} alt="eye" /></div><span> 1359</span></div>
+                                <p>Followers <span></span></p>
                             </div>
                         </div>
-                        <div></div>
                     </div>
+                    <div></div>
                     <div></div>
                 </div>
             </div>
