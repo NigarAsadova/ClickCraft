4efcc8a6653c497d0ae68d72681f4a62e67100b9
NigarAsadova <nigar.asadova555@gmail.com>
2025-03-08 23:13:44 +0400

    Add font sizes and colors to styling.

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -6,14 +6,14 @@ const Community = () => {
             <div>
                 <div>
                     <div className="p-5 border-[#664DFF] border-t-2 border-x border-b-4 rounded-[10px] flex flex-col items-">
-                        <div className="text-[#77798F]">
+                        <div className="text-[#77798F] text-xl font-normal">
                             Author of "Python 3.0 for Beginners" and a Full Stack Developer passionate about creating dynamic and user-friendly web experiences.
                         </div>
-                        <div className="text-white">
-                            <p>Portfolios <span>10</span></p>
-                            <div>
-                                <div className="flex gap-[6px]"><div><img src={eye} alt="eye" /></div><span> 1359</span></div>
-                                <p>Followers <span></span></p>
+                        <div className="text-white text-xl font-normal flex justify-between">
+                            <p>Portfolios <span className="font-bold">10</span></p>
+                            <div className="flex gap-1">
+                                <div className="flex gap-[6px]"><div><img src={eye} alt="eye" /></div><span className="font-bold"> 1359</span></div>
+                                <p>Followers <span className="font-bold">999</span></p>
                             </div>
                         </div>
                     </div>
